302b3bd8739b88c0e48579421904ddaf839135bc
svetlana18055 <svetlana18055@yandex.ru>
2023-09-11 02:00:50 +0300

    ToDoList, 7 lesson 404 page

--- src/components/Header/Header.styled.ts
+++ src/components/Header/Header.styled.ts
@@ -52,6 +52,7 @@ export const SwitchThemeField = styled.input`
         margin-top: 0.15em; 
         appearance: none;
         transition: all 0.15s;
+        cursor: pointer;
         
         &:checked {
             background: #0d6efd url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'%3e%3ccircle r='3' fill='%23fff'/%3e%3c/svg%3e") right center no-repeat;
